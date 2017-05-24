f4a5b031f7bb3eb1b7445de0562f5c31fdf174cf
Roman <rkryzhanskyy@amcbridge.com>
2017-05-24 16:33:26 +0300

    Add test console application. Add test library. Add installer for Test Project.

---
+++ TestConsoleAppliaction/TestClassLibrary/SomeWorkRep.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace TestClassLibrary
+{
+    public static class SomeWorkRep
+    {
+        public static string ReversedUserData(string userName)
+        {
+            return new string(userName.Reverse().ToArray());
+        }
+    }
+}
---
+++ TestConsoleAppliaction/TestConsoleAppliaction/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TestClassLibrary;
+
+namespace TestConsoleAppliaction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellocome to Test Console Application:");
+            Console.WriteLine("Input user name:");
+
+            var userName = Console.ReadLine();
+            Console.WriteLine("Hello " + userName);
+
+            Console.WriteLine("Reversed user data:");
+            Console.WriteLine(SomeWorkRep.ReversedUserData(userName));
+
+            Console.ReadKey();
+        }
+    }
+}
